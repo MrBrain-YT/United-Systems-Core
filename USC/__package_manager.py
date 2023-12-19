c66c366518d61a717ffa7ac59017e92bf4380c76
Ivan <109616511+MrBrain-YT@users.noreply.github.com>
2023-12-19 13:01:10 +0300

    create update
    add:
    - create

--- USC/__package_manager.py
+++ USC/__package_manager.py
@@ -1,7 +1,8 @@
 import os
 import time
+from typing import Any
 
-from progress.bar import Bar
+from progress.bar import ShadyBar
 
 class PackageManager():
     
@@ -9,14 +10,18 @@ class PackageManager():
         pass
     
     def install(self, name:str) -> None:
+        self.create(name=name)
         name = name.lower()
-        with Bar('Processing', max=20) as bar:
+        with ShadyBar('Installing', max=20) as bar:
             for i in range(20):
                 time.sleep(0.5)
                 bar.next()
                    
-        with open(f"packages/{name}.pkg", "w") as file:
+        with open(f"packages/{name}/{name}.pkg", "w") as file:
             file.write("package installed")
             
     def uninstall(self, name:str) -> None:
         os.remove(f"packages/{name.lower()}.pkg")
+        
+    def create(self, name:str) -> None:
+        os.makedirs(f"packages/{name}")
--- USC/usc.py
+++ USC/usc.py
@@ -7,19 +7,20 @@ from __package_manager import PackageManager
 sys_args = sys.argv[1::]
 Manager = PackageManager()
 
+
 match sys_args[0]:
     
     case "install":
         Manager.install(name=sys_args[1])
-    
+            
     case "uninstall":
         Manager.uninstall(name=sys_args[1])
     
-    case "refrash":
+    case "refresh":
         pass
     
     case "create":
-        pass
+        Manager.create(name=sys_args[1])
     
     case "list":
         pass
@@ -38,3 +39,10 @@ match sys_args[0]:
     
     case "-h":
         pass
+    
+    case "-v":
+        print("Current United Systems Core version: 0.0.1")
+
+    case _:
+        print("Unknown command")
+            
