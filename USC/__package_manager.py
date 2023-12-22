9eb52f91dc02dd7307fe45f3786f16cac6611be0
Ivan <109616511+MrBrain-YT@users.noreply.github.com>
2023-12-22 12:31:56 +0300

    Refresh update
    Add:
    - updated refresh command

--- USC/__package_manager.py
+++ USC/__package_manager.py
@@ -10,10 +10,11 @@ from progress.bar import ShadyBar
 class PackageManager():
     
     def __init__(self) -> None:
-        self.list = ListWorker(path=f"{os.path.dirname(os.path.abspath(__file__))}/__packages")
+        self.current_directory = os.path.dirname(os.path.abspath(__file__))
+        self.list = ListWorker(path=f"{self.current_directory}/__packages")
     
     def install(self, name:str) -> None:
-        with open(f"{os.path.dirname(os.path.abspath(__file__))}/__packages", "r") as file:
+        with open(f"{self.current_directory}/__packages", "r") as file:
             text = file.readlines()
             lines = []
             for line in text:
@@ -27,28 +28,35 @@ class PackageManager():
                         time.sleep(0.5)
                         bar.next()
                         
-                os.makedirs(f"{os.path.dirname(os.path.abspath(__file__))}/packages/{name}")
-                with open(f"{os.path.dirname(os.path.abspath(__file__))}/packages/{name}/{name}.pkg", "w") as file:
+                os.makedirs(f"{self.current_directory}/packages/{name}")
+                with open(f"{self.current_directory}/packages/{name}/{name}.pkg", "w") as file:
                     file.write("package installed")
-                self.list.add_package_to_list(name=name, version="1.0.0")
+                    
+                package_version = "1.0.0"
+                with open(f"{self.current_directory}/packages/{name}/version", "w") as version_file:
+                    version_file.write(package_version)
+                self.list.add_package_to_list(name=name, version=package_version)
                 print(f"Library {name} successful installed.")
             
     def uninstall(self, name:str) -> None:
         self.list.remove_package_from_list(name=name)
-        shutil.rmtree(f"{os.path.dirname(os.path.abspath(__file__))}/packages/{name}")
+        shutil.rmtree(f"{self.current_directory}/packages/{name}")
         
     def create(self, name:str) -> None:
-        self.list.add_package_to_list(name=name, version="0.0.1")
-        os.makedirs(f"{os.path.dirname(os.path.abspath(__file__))}/packages/{name}")
+        package_version = "0.0.1"
+        self.list.add_package_to_list(name=name, version=package_version)
+        os.makedirs(f"{self.current_directory}/packages/{name}")
+        with open(f"{self.current_directory}/packages/{name}/version", "w") as version_file:
+            version_file.write(package_version)
 
     def get_list(self) -> str:
-        with open(f"{os.path.dirname(os.path.abspath(__file__))}/__packages", "r") as file:
+        with open(f"{self.current_directory}/__packages", "r") as file:
             text = file.read()
         return text 
     
     def set_server_config(self, server_info:str, is_my_server:bool) -> None:
         config = configparser.ConfigParser()
-        config.read(f'{os.path.dirname(os.path.abspath(__file__))}/run.ini')
+        config.read(f'{self.current_directory}/run.ini')
         if is_my_server:
             host, port = server_info.split(":")
             config['SERVER'] = {'host': host,
@@ -63,6 +71,21 @@ class PackageManager():
                 config.write(configfile)
                 
     def refresh(self) -> None:
-        files = os.listdir(f"{os.path.dirname(os.path.abspath(__file__))}/packages")
-        with open(f"{os.path.dirname(os.path.abspath(__file__))}/__packages", "w") as file:
-            file.write("\n".join(files))
+        # get files from directory
+        files = os.listdir(f"{self.current_directory}/packages")
+        versions = []
+        for file in files:
+            if os.path.exists(f"{self.current_directory}/packages/{file}/version"):
+                with open(f"{self.current_directory}/packages/{file}/version", "r") as file_version:
+                    versions.append(file_version.read())
+            else:
+                versions.append("0.0.1")
+                with open(f"{self.current_directory}/packages/{file}/version", "w") as file_version:
+                    file_version.write("0.0.1")
+        # join flename and version
+        file_and_version = []
+        for index, file in enumerate(files):
+            file_and_version.append(f"{file}=={versions[index]}")
+            
+        with open(f"{self.current_directory}/__packages", "w") as file:
+            file.write("\n".join(file_and_version))
