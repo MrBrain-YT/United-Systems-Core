4f4d485df134195031339b225a4f346c4f3ab09a
Ivan <109616511+MrBrain-YT@users.noreply.github.com>
2023-12-26 15:54:05 +0300

    Installing package update
    Add:
    - installing pakcages from USCServer

--- USC/__list_work.py
+++ USC/__list_work.py
@@ -1,30 +1,33 @@
 import re
+import os
+import configparser
 
 class ListWorker():
 
     def __init__(self, path:str) -> None:
         self.__path = path
 
-    def add_package_to_list(self, name:str, version:str) -> None:
-        with open(self.__path, "r") as file:
-            text = file.read()
-        if text != "":     
-            with open(self.__path, "a") as file:
-                file.write("\n" + name + f"=={version}")
-        else:
-            with open(self.__path, "a") as file:
-                file.write(name + f"=={version}")
+    def add_package_to_list(self, package_config:configparser.ConfigParser) -> None:
+        dir_path = f"{os.path.dirname(os.path.abspath(__file__))}/packages"
+        package_name = package_config["INFO"].get("name")
+        package_version = package_config["INFO"].get("version")
+        
+        # Add package to package list
+        config = configparser.ConfigParser()
+        config.read(f"{dir_path}/packages.ini")
+        config[package_name.lower()] = {
+            "name" : package_name,
+            "version" :package_version
+        }
+        with open(f"{dir_path}/packages.ini", 'w') as configfile:
+            config.write(configfile)
 
     def remove_package_from_list(self, name:str) -> None:
-        with open(self.__path, "r") as file:
-            text = file.readlines()
-            lines = []
-            for line in text:
-                if line == "\n":
-                    continue
-                elif re.search(name+r"==\d.\d.\d", string=line):
-                    continue
-                else:
-                    lines.append(line)
-            with open(self.__path, "w") as file:
-                file.write("".join(lines))
+        dir_path = f"{os.path.dirname(os.path.abspath(__file__))}/packages"
+        config = configparser.ConfigParser()
+        config.read(f"{dir_path}/packages.ini")
+        if config.has_section(name):
+            config.remove_section(name)
+        
+        with open(f"{dir_path}/packages.ini", 'w') as configfile:
+            config.write(configfile)
--- USC/__package_manager.py
+++ USC/__package_manager.py
@@ -3,6 +3,9 @@ import time
 import re
 import configparser
 import shutil
+import requests
+import os
+import tarfile
 
 from __list_work import ListWorker
 from progress.bar import ShadyBar
@@ -14,43 +17,49 @@ class PackageManager():
         self.list = ListWorker(path=f"{self.current_directory}/__packages")
     
     def install(self, name:str) -> None:
-        with open(f"{self.current_directory}/__packages", "r") as file:
-            text = file.readlines()
-            lines = []
-            for line in text:
-                if re.search(name+r"==\d.\d.\d", string=line):
-                    print("Package is already present")
-                    break
-            else:
-                name = name.lower()
-                with ShadyBar('Installing', max=20) as bar:
-                    for i in range(20):
-                        time.sleep(0.5)
-                        bar.next()
-                        
-                os.makedirs(f"{self.current_directory}/packages/{name}")
-                with open(f"{self.current_directory}/packages/{name}/{name}.pkg", "w") as file:
-                    file.write("package installed")
-                    
-                package_version = "1.0.0"
-                with open(f"{self.current_directory}/packages/{name}/version", "w") as version_file:
-                    version_file.write(package_version)
-                self.list.add_package_to_list(name=name, version=package_version)
-                print(f"Library {name} successful installed.")
+        name = name.lower()
+        
+        # Get package
+        data = {
+            "package_name" : name
+            }
+        responce = requests.post(url="http://127.0.0.1:5000/package", data=data)
+        with open(f'{os.path.dirname(os.path.abspath(__file__))}/temp/{name}.tar.gz', 'wb') as file:
+            file.write(responce.content)
+        
+        # extracting file     
+        dir_path = f"{os.path.dirname(os.path.abspath(__file__))}/packages"
+        file = tarfile.open(f"{os.path.dirname(os.path.abspath(__file__))}/temp/{name}.tar.gz") 
+        file.extractall(dir_path) 
+        file.close()
+        os.remove(f'{os.path.dirname(os.path.abspath(__file__))}/temp/{name}.tar.gz')
+        
+        # Get properties from package.ini
+        package_config = configparser.ConfigParser()
+        package_config.read(f"{dir_path}/{name}/package.ini")
+        
+        with ShadyBar('Installing', max=20) as bar:
+            for i in range(20):
+                time.sleep(0.1)
+                bar.next()
+                
+        self.list.add_package_to_list(package_config=package_config)
+    
             
     def uninstall(self, name:str) -> None:
         self.list.remove_package_from_list(name=name)
         shutil.rmtree(f"{self.current_directory}/packages/{name}")
         
+    # Not worked 
     def create(self, name:str) -> None:
         package_version = "0.0.1"
-        self.list.add_package_to_list(name=name, version=package_version)
-        os.makedirs(f"{self.current_directory}/packages/{name}")
-        with open(f"{self.current_directory}/packages/{name}/version", "w") as version_file:
-            version_file.write(package_version)
+        # self.list.add_package_to_list(name=name, version=package_version)
+        # os.makedirs(f"{self.current_directory}/packages/{name}")
+        # with open(f"{self.current_directory}/packages/{name}/version", "w") as version_file:
+        #     version_file.write(package_version)
 
     def get_list(self) -> str:
-        with open(f"{self.current_directory}/__packages", "r") as file:
+        with open(f"{self.current_directory}/packages/packages.ini", "r") as file:
             text = file.read()
         return text 
     
@@ -69,23 +78,24 @@ class PackageManager():
                                 'port': port}
             with open('run.ini', 'w') as configfile:
                 config.write(configfile)
-                
+            
+    # Not worked     
     def refresh(self) -> None:
         # get files from directory
         files = os.listdir(f"{self.current_directory}/packages")
-        versions = []
-        for file in files:
-            if os.path.exists(f"{self.current_directory}/packages/{file}/version"):
-                with open(f"{self.current_directory}/packages/{file}/version", "r") as file_version:
-                    versions.append(file_version.read())
-            else:
-                versions.append("0.0.1")
-                with open(f"{self.current_directory}/packages/{file}/version", "w") as file_version:
-                    file_version.write("0.0.1")
-        # join flename and version
-        file_and_version = []
-        for index, file in enumerate(files):
-            file_and_version.append(f"{file}=={versions[index]}")
+        # versions = []
+        # for file in files:
+        #     if os.path.exists(f"{self.current_directory}/packages/{file}/version"):
+        #         with open(f"{self.current_directory}/packages/{file}/version", "r") as file_version:
+        #             versions.append(file_version.read())
+        #     else:
+        #         versions.append("0.0.1")
+        #         with open(f"{self.current_directory}/packages/{file}/version", "w") as file_version:
+        #             file_version.write("0.0.1")
+        # # join flename and version
+        # file_and_version = []
+        # for index, file in enumerate(files):
+        #     file_and_version.append(f"{file}=={versions[index]}")
             
-        with open(f"{self.current_directory}/__packages", "w") as file:
-            file.write("\n".join(file_and_version))
+        # with open(f"{self.current_directory}/__packages", "w") as file:
+        #     file.write("\n".join(file_and_version))
